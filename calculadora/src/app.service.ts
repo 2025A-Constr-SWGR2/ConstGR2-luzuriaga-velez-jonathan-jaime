de13ce022b41df46ea837541c7000aacc7b77ec8
Jonathan Luzuriaga <jonathan.luzuriaga@epn.edu.ec>
2025-05-30 09:08:03 -0500

    Created dockerfile and create images

--- calculadora/src/app.service.ts
+++ calculadora/src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.warn('warn!');
+    console.error('error!');
     return 'Hello World!';
   }
 }
